5f9702113620643f0e7be47f7c35f8bf7c617327
chrisxkoch <52881970+chrisxkoch@users.noreply.github.com>
2019-12-05 12:47:42 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,269 +1,268 @@
-const express = require('express'),
-morgan = require('morgan'),
-bodyParser = require('body-parser'),
-uuid = require('uuid');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
 
-let Users = [
-  {id: '0', name: 'Chris', username: 'chrisxkoch', password: '', email: '', birthday: '', favorites: ['3']}
-];
+const Movies = Models.Movie;
+const Users = Models.User;
+const cors = require("cors");
+const { check, validationResult } = require("express-validator");
+const passport = require("passport");
 
-let Directors = [ 
-  {name: 'Christopher Nolan', bio: '', birthyear: '', deathyear: ''},
-  {name: 'Steven Spielberg', bio: '', birthyear: '', deathyear: ''},
-  {name: 'Ridley Scott', bio: '', birthyear: '', deathyear: ''},
-  {name: 'Alex Proyas', bio: '', birthyear: '', deathyear: ''},
-  {name: 'Mel Brooks', bio: '', birthyear: '', deathyear: ''},
-  {name: 'Stanley Kubrick', bio: '', birthyear: '', deathyear: ''},
-  {name: 'Andrew Niccol', bio: '', birthyear: '', deathyear: ''},
-];
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true })
 
-let Genres = [ 
-  { name: 'SciFi', description: 'Science fiction (often abbreviated Sci-Fi or SF) is a genre of speculative fiction that has been called the "literature of ideas". It typically deals with imaginative and futuristic concepts such as advanced science and technology, time travel, parallel universes, fictional worlds, space exploration, and extraterrestrial life. Science fiction often explores the potential consequences of scientific innovations.'},
-  { name: 'Crime', description: 'Crime films, in the broadest sense, are a cinematic genre inspired by and analogous to the crime fiction literary genre. Films of this genre generally involve various aspects of crime and its detection.'},
-  { name: 'Drama', description: 'Drama is a genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone'}
-];
+// Middleware functions
+app.use(express.static("public"));
+app.use(morgan("common")); // Logging with Morgan
+app.use(bodyParser.json()); // Using bodyParser
+app.use(cors()); // Using cors
 
-let Movies = [ 
-  {
-    id: '0',
-    title : 'Inception',
-    year : '2010',
-    description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
-    genre: 'SciFi',
-    director: 'Christopher Nolan',
-    imageURL: '',
-    featured: 'false'
-  },
-  {
-    id: '1',
-    title : 'Minority Report',
-    year : '2002',
-    description: 'In a future where a special police unit is able to arrest murderers before they commit their crimes, an officer from that unit is himself accused of a future murder.',
-    genre: 'Crime',
-    director: 'Steven Spielberg',
-    imageURL: 'https://m.media-amazon.com/images/M/MV5BZTI3YzZjZjEtMDdjOC00OWVjLTk0YmYtYzI2MGMwZjFiMzBlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX140_CR0,0,140,209_AL_.jpg',
-    featured: 'false'
-  },
-  {
-    id: '2',
-    title : 'Interstellar',
-    year : '2014',
-    description: 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.',
-    genre: 'Drama',
-    director: 'Christopher Nolan',
-    imageURL: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY209_CR0,0,140,209_AL_.jpg',
-    featured: 'false'
-  },
-  {
-    id: '3',
-    title : 'Donnie Darko',
-    year : '2001',
-    description: 'A troubled teenager is plagued by visions of a man in a large rabbit suit who manipulates him to commit a series of crimes, after he narrowly escapes a bizarre accident.',
-    genre: 'Drama',
-    director: 'Christopher Nolan',
-    imageURL: 'https://m.media-amazon.com/images/M/MV5BZjZlZDlkYTktMmU1My00ZDBiLWFlNjEtYTBhNjVhOTM4ZjJjXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX140_CR0,0,140,209_AL_.jpg',
-    featured: 'false'
-  },
-  {
-    id: '4',
-    title : 'The Martian',
-    year : '2015',
-    description: 'An astronaut becomes stranded on Mars after his team assume him dead, and must rely on his ingenuity to find a way to signal to Earth that he is alive.',
-    genre: 'Drama',
-    director: 'Ridley Scott',
-    imageURL: 'https://m.media-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_UY209_CR0,0,140,209_AL_.jpg',
-    featured: 'false'
-  },
-  {
-    id: '5',
-    title : 'Dark City',
-    year : '1998',
-    description: '',
-    genre: 'SciFi',
-    director: 'Alex Proyas',
-    imageURL: '',
-    featured: 'false'
-  },
-  {
-    id: '6',
-    title : 'Spaceballs',
-    year : '1987',
-    description: '',
-    genre: '',
-    director: 'Mel Brooks',
-    imageURL: '',
-    featured: 'false'
-  },
-  {
-    id: '7',
-    title : '2001: A Space Odyssey',
-    year : '1968',
-    description: '',
-    genre: 'SciFi',
-    director: 'Stanley Kubrick',
-    imageURL: '',
-    featured: 'false'
-  },
-  {
-    id: '8',
-    title : 'Gattaca',
-    year : '1997',
-    description: '',
-    genre: 'SciFi',
-    director: 'Andrew Niccol',
-    imageURL: '',
-    featured: 'false'
-  },
-  {
-    id: '9',
-    title : 'Blade Runner',
-    year : '1982',
-    description: '',
-    genre: 'SciFi',
-    director: 'Ridley Scott',
-    imageURL: '',
-    featured: 'false'
-  }
-];
+var auth = require("./auth")(app);
 
-// use morgan logger middleware
-app.use(morgan('common'));
+//Error handling middleware functions
 
-// routes all requests for static files to 'public' folder
-app.use(express.static('public'));
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+  next();
+});
 
-app.use(bodyParser.json());
+// Homepage
 
-// GET requests
-app.get('/', (req, res) => {
-  res.redirect('/index.html');
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix!");
 });
 
 // -- Movies --
+// Gets the list of data about ALL movies
 
-// Get the list of data about all Movies
-app.get('/movies', (req, res) => {
-  res.json(Movies);
-});
+app.get(
+  "/movies",
+  (req, res) => {
+    Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
 
-// Get the data about a single Movie, by title
-app.get('/movies/:title', (req, res) => {
-  res.json(Movies.find( (movie) => { return movie.title.toLowerCase().includes(req.params.title.toLowerCase()); }));
-});
+// Gets the data about a single movie, by title
 
-// -- Genres --
+app.get(
+  "/movies/:movieId",
+  (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+      .then((movie) => {
+        res.json(movie);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
 
-// Get the data about a single Genre, by name
-app.get('/genres/:name', (req, res) => {
-  res.json(Genres.find( (genre) => { return genre.name === req.params.name; }));
-});
+// Gets the data about a movie genre, by name
 
-// -- Directors --
+app.get(
+  "/movies/genres/:Name",
+  (req, res) => {
+    Movies.findOne({
+      "Genre.Name": req.params.Name
+    })
+      .then((movies) => {
+        res.json(movies.Genre);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
 
-// Get the data about a single Director, by name
-app.get('/directors/:name', (req, res) => {
-  res.json(Directors.find( (director) => { return director.name === req.params.name; }));
-});
+
+// Gets data about a director
+
+app.get(
+  "/movies/directors/:Name",
+  (req, res) => {
+    Movies.findOne({
+      "Director.Name": req.params.Name
+    })
+      .then((movies) => {
+        res.json(movies.Director);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
 
 // -- Users --
+// Add a user
 
-// Get the list of data about all Movies
-app.get('/users', (req, res) => {
-  res.json(Users);
-});
-
-// Adds data for a new user to the list of Users.
-app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    Users.push(newUser);
-    res.status(201).send(newUser);
+app.post(
+  "/users",
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non alphanumeric characters - not allowed"
+    ).isAlphanumeric(),
+    check("Password", "Password is required")
+      .not()
+      .isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail()
+  ],
+  (req, res) => {
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    var hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + " already exists");
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+            .then((user) => {
+              res.status(201).json(user);
+            })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send("Error: " + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
   }
-});
+);
 
-// Deletes a user from the list by ID
-app.delete('/users/:id', (req, res) => {
-  let user = Users.find((user) => { return user.id === req.params.id; });
+// Update the user's information
 
-  if (user) {
-    Users = Users.filter(function(obj) { return obj.id !== req.params.id; });
-    res.status(201).send('User ' + user.name + ' with id ' + req.params.id + ' was deleted.')
+app.put(
+  "/users/:Username",
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non alphanumeric characters - not allowed"
+    ).isAlphanumeric(),
+    check("Password", "Password is required")
+      .not()
+      .isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail()
+  ],
+  (req, res) => {
+    var errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    var hashedPassword = Users.hashPassword(req.body.Password);
+    Users.update(
+      { Username: req.params.Username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }
+      },
+      { new: true }, // This line makes sure that the updated document is returned
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Error: " + error);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
   }
-});
+);
 
-// Get a user from the list by ID
-app.get('/users/:id', (req, res) => {
-  res.json(Users.find( (user) => { return user.id === req.params.id; }));
-});
+// Add a movie to a user's list of favorites
 
-// Update the info of a user by id
-app.put('/users/:id', (req, res) => {
-  let user = Users.find((user) => { return user.id === req.params.id; });
-  let newUserInfo = req.body;
-
-  if (user && newUserInfo) {
-    // preserve the user id
-    newUserInfo.id = user.id;
-    // preserve the user favorites
-    newUserInfo.favorites = user.favorites;    
-    // merge old info and new info (TODO: validate new info)
-    Object.assign(user, newUserInfo);
-    // merge user with update info into the list of Users
-    Users = Users.map((user) => (user.id === newUserInfo.id) ? newUserInfo : user);
-    res.status(201).send(user);
-  } else if (!newUserInfo.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
-  } else {
-    res.status(404).send('User with id ' + req.params.id + ' was not found.');
-  } 
-});
-
-// -- List of Favorites --
-
-// add a favorite Movie to a User.
-app.post('/users/:id/:movie_id', (req, res) => {
-  let user = Users.find((user) => { return user.id === req.params.id; });
-  let movie = Movies.find((movie) => { return movie.id === req.params.movie_id; });
-
-  if (user && movie) {
-    user.favorites = [...new Set([...user.favorites, req.params.movie_id])];
-    res.status(201).send(user);
-  } else if (!movie) {
-    res.status(404).send('Movie with id ' + req.params.movie_id + ' was not found.');
-  } else {
-    res.status(404).send('User with id ' + req.params.id + ' was not found.');
+app.post(
+  "/users/:Username/Movies/:MovieID",
+  (req, res) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      { $push: { FavoriteMovies: req.params.MovieID } },
+      { new: true }, // This line makes sure that the updated document is returned
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Error: " + error);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
   }
-});
+);
 
-// remove a favorite Movie from a User.
-app.delete('/users/:id/:movie_id', (req, res) => {
-  let user = Users.find((user) => { return user.id === req.params.id; });
-  let movie = Movies.find((movie) => { return movie.id === req.params.movie_id; });
+// Remove a movie from user's list of favorites
 
-  if (user && movie) {
-    user.favorites = user.favorites.filter((movie_id) => { return movie_id !== req.params.movie_id; });
-    res.status(201).send(user);
-  } else if (!movie) {
-    res.status(404).send('Movie with id ' + req.params.movie_id + ' was not found.');
-  } else {
-    res.status(404).send('User with id ' + req.params.id + ' was not found.');
+app.delete(
+  "/users/:Username/Movies/:MovieID",
+  (req, res) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      { $pull: { FavoriteMovies: req.params.MovieID } },
+      { new: true }, // This line makes sure that the updated document is returned
+      (error, updatedUser) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Error: " + error);
+        } else {
+          res.json(updatedUser);
+        }
+      }
+    );
   }
-});
+);
 
-app.use((err, req, res, next) => {
-  var logEntryTimestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
-  var logEntry = `${logEntryTimestamp} - Error: ${err.stack}`;
-  console.error(logEntry);
-  res.status(500).send('Something broke!');
-});
+// Delete a user by username
 
-app.listen(8080, () => {
-  console.log('Movie API is listening on port 8080');
+app.delete(
+  "/users/:Username",
+  (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+      .then((user) => {
+        if (!user) {
+          res.status(400).send(req.params.Username + " was not found");
+        } else {
+          res.status(200).send(req.params.Username + " was deleted.");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      });
+  }
+);
+
+// Listen for requests on port 8080
+
+var port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 8080");
 });
