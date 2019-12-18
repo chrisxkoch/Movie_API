719e913721f0a0813a392226b44142b16afef879
chrisxkoch <chrxkoch@icloud.com>
2019-12-18 13:09:57 +0100

    new edit movie funtion

--- index.js
+++ index.js
@@ -68,6 +68,34 @@ app.post("/movies",
   }
 );
 
+// Update Movie
+app.put(
+  "/movies/:Title",
+  (req, res) => {
+    Movies.update(
+      { Title: req.params.Title },
+      {
+        $set: {
+          Title: req.body.Title,
+          Description: req.body.Description,
+          Genre: req.body.Genre,
+          Director: req.body.Director,
+          Imagepath: req.body.Imagepath
+        }
+      },
+      { new: true }, // This line makes sure that the updated document is returned
+      (error, updatedMovie) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send("Error: " + error);
+        } else {
+          res.json(updatedMovie);
+        }
+      }
+    );
+  }
+);
+
 // Gets the list of data about ALL movies
 
 app.get(
