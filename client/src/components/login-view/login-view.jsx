44b3c79cd4ba188347363fa283b23a162d8b4bdc
chrisxkoch <52881970+chrisxkoch@users.noreply.github.com>
2019-12-23 14:59:32 +0100

    Task 3.4

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,59 +1,79 @@
-import React, { useState } from "react";
-import "./login-view.scss";
-import axios from "axios";
-import { Button, Form } from "react-bootstrap";
-import logo from "../../images/logo2.png";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+// imports for files to bundle
+import './login-view.scss';
 
 export function LoginView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ validated, setValidated] = useState(false);
 
   const handleSubmit = (e) => {
+
     e.preventDefault();
-    /* Send a request to the server for authentication */
-    axios
-      .post("https://myflixdb-1988.herokuapp.com/login", {
-        Username: username,
-        Password: password
-      })
-      .then((response) => {
-        const data = response.data;
-        props.onLoggedIn(username);
-      })
-      .catch((e) => {
-        console.log("no such user");
-      });
+
+    // handles form validation
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      console.log('new login', username, 'with password', password);
+      // this is temporary, always accept username/password
+      props.onLoggedIn(username);      
+    }
+    // notify that fields were validated,
+    // therefore feedback can be shown
+    // (otherwise it will appear at page load)
+    setValidated(true);
   };
 
   return (
-    <Form className="login-form">
-      <img src={logo} alt="logo" style={{ width: "300px" }} />
-      <Form.Group controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type="username"
-          placeholder="Enter username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </Form.Group>
-      <Button
-        variant="btn-lg btn-dark btn-block"
-        type="submit"
-        onClick={handleSubmit}
-      >
-        Login
-      </Button>
-    </Form>
+    <div className="login-view">
+      <Row className="justify-content-center">
+        <Col xs={11} sm={8} md={6} className="form-container">
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control 
+                type="text" 
+                value={username}
+                onChange={ e => setUsername(e.target.value)}      
+                required
+                placeholder="Enter username" />
+              <Form.Control.Feedback type="invalid">
+                Please choose a username.
+              </Form.Control.Feedback>            
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type="password"
+                value={password}
+                onChange={ e => setPassword(e.target.value)}
+                required              
+                placeholder="Password" />
+              <Form.Control.Feedback type="invalid">
+                Please insert your password.
+              </Form.Control.Feedback>                
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </div>
   );
-}
+};
+
+LoginView.propTypes = {
+  onLoggedIn: PropTypes.func.isRequired
+};
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { MainView } from "./components/main-view/main-view";
+// imports for files to bundle
+import './index.scss';
 
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
+// import components
+import { MainView } from './components/main-view/main-view';
 
-// Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component {
+// main component
+class SoFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (<MainView /> );
   }
 }
 
-// Finds the root of your app
-const container = document.getElementsByClassName("app-container")[0];
+// root element of the app
+const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+// set app root DOM element
+ReactDOM.render(React.createElement(SoFlixApplication), container);
